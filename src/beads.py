0ad30f46649c7654a44652669b6f689fa377fb6c
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-10-28 13:03:08 +0200

    Working on bead to potential conversion.

--- src/beads.py
+++ src/beads.py
@@ -11,6 +11,10 @@ Author: Andrew Tarzia
 
 from dataclasses import dataclass
 from typing import Union, Tuple
+import itertools
+import logging
+
+from gulp_optimizer import HarmBond, ThreeAngle, CheckedThreeAngle
 
 
 @dataclass
@@ -21,16 +25,16 @@ class CgBead:
     angle_centered: Union[float, Tuple[float]]
 
 
-# Mn = CgBead("Mn", sigma=2.0, connectivity=0, angle_centered=180)
+# Mn = CgBead("F", sigma=2.0, connectivity=0, angle_centered=180)
 # Hg = CgBead("Hg", sigma=2.0, connectivity=0, angle_centered=180)
 # Mo = CgBead("Mo", sigma=2.0, connectivity=0, angle_centered=180)
 # Nd = CgBead("Nd", sigma=2.0, connectivity=0, angle_centered=180)
 # Ne = CgBead("Ne", sigma=2.0, connectivity=0, angle_centered=180)
-# Ni = CgBead("Ni", sigma=2.0, connectivity=0, angle_centered=180)
+# Ni = CgBead("Cs", sigma=2.0, connectivity=0, angle_centered=180)
 # Nb = CgBead("Nb", sigma=2.0, connectivity=0, angle_centered=180)
 # N = CgBead("N", sigma=2.0, connectivity=0, angle_centered=180)
 # Os = CgBead("Os", sigma=2.0, connectivity=0, angle_centered=180)
-# O = CgBead("O", sigma=2.0, connectivity=0, angle_centered=180)
+# O = CgBead("Li", sigma=2.0, connectivity=0, angle_centered=180)
 # Pd = CgBead("Pd", sigma=2.0, connectivity=0, angle_centered=180)
 # P = CgBead("P", sigma=2.0, connectivity=0, angle_centered=180)
 # Pt = CgBead("Pt", sigma=2.0, connectivity=0, angle_centered=180)
@@ -38,26 +42,26 @@ class CgBead:
 # CgBead("H", sigma=2.0, connectivity=3, angle_centered=60),
 # CgBead("Kr", sigma=2.0, connectivity=3, angle_centered=60),
 # CgBead("La", sigma=2.0, connectivity=3, angle_centered=60),
-# CgBead("Pb", sigma=2.0, connectivity=3, angle_centered=60),
-# CgBead("Li", sigma=2.0, connectivity=3, angle_centered=60),
+# CgBead("Dy", sigma=2.0, connectivity=3, angle_centered=60),
+# CgBead("Cl", sigma=2.0, connectivity=3, angle_centered=60),
 # CgBead("Lu", sigma=2.0, connectivity=3, angle_centered=60),
 
 
-def beads_1c():
+def core_beads():
     return (
-        CgBead("Cl", sigma=3.0, connectivity=1, angle_centered=40),
+        CgBead("O", sigma=3.0, connectivity=1, angle_centered=40),
         CgBead("Cr", sigma=3.0, connectivity=1, angle_centered=60),
         CgBead("Co", sigma=3.0, connectivity=1, angle_centered=90),
         CgBead("Cu", sigma=3.0, connectivity=1, angle_centered=120),
-        CgBead("Dy", sigma=3.0, connectivity=1, angle_centered=140),
+        CgBead("Pb", sigma=3.0, connectivity=1, angle_centered=140),
         CgBead("Er", sigma=3.0, connectivity=1, angle_centered=160),
         CgBead("Eu", sigma=3.0, connectivity=1, angle_centered=180),
-        CgBead("F", sigma=4.0, connectivity=1, angle_centered=40),
+        CgBead("Mn", sigma=4.0, connectivity=1, angle_centered=40),
         CgBead("Gd", sigma=4.0, connectivity=1, angle_centered=60),
         CgBead("Ga", sigma=4.0, connectivity=1, angle_centered=90),
         CgBead("Ge", sigma=4.0, connectivity=1, angle_centered=120),
         CgBead("Au", sigma=4.0, connectivity=1, angle_centered=140),
-        CgBead("Cs", sigma=4.0, connectivity=1, angle_centered=160),
+        CgBead("Ni", sigma=4.0, connectivity=1, angle_centered=160),
         CgBead("He", sigma=4.0, connectivity=1, angle_centered=180),
     )
 
@@ -83,13 +87,90 @@ def beads_2c():
 
 def beads_3c():
     return (
-        CgBead("Ho", sigma=2.0, connectivity=3, angle_centered=60),
-        CgBead("Fe", sigma=2.5, connectivity=3, angle_centered=60),
-        CgBead("In", sigma=3.0, connectivity=3, angle_centered=60),
-        CgBead("I", sigma=3.5, connectivity=3, angle_centered=60),
-        CgBead("Ir", sigma=4.0, connectivity=3, angle_centered=60),
+        CgBead("Ho", sigma=2.0, connectivity=3, angle_centered=120),
+        CgBead("Fe", sigma=2.5, connectivity=3, angle_centered=120),
+        CgBead("In", sigma=3.0, connectivity=3, angle_centered=120),
+        CgBead("I", sigma=3.5, connectivity=3, angle_centered=120),
+        CgBead("Ir", sigma=4.0, connectivity=3, angle_centered=120),
     )
 
 
 def beads_4c():
-    return (CgBead("Pt", 2.0, 4, (90, 180)),)
+    return (CgBead("Pt", 2.0, 4, (90, 180, 130)),)
+
+
+def lorentz_berthelot_sigma_mixing(sigma1, sigma2):
+    return (sigma1 + sigma2) / 2
+
+
+def bond_library():
+    logging.info("you are not yet assigning different k values")
+    bond_k = 10
+
+    all_beads = core_beads() + beads_2c() + beads_3c() + beads_4c()
+
+    bonds = [
+        HarmBond(
+            atom1_type=i.element_string,
+            atom2_type=i.element_string,
+            bond_r=i.sigma,
+            bond_k=bond_k,
+        )
+        for i in all_beads
+    ]
+    for pair in itertools.combinations(all_beads, r=2):
+        sorted_ids = tuple(
+            sorted([pair[0].element_string, pair[1].element_string])
+        )
+        mixed_radii = lorentz_berthelot_sigma_mixing(
+            sigma1=pair[0].sigma,
+            sigma2=pair[1].sigma,
+        )
+        bonds.append(
+            HarmBond(
+                atom1_type=sorted_ids[0],
+                atom2_type=sorted_ids[1],
+                bond_r=mixed_radii,
+                bond_k=bond_k,
+            )
+        )
+
+    return tuple(bonds)
+
+
+def angle_library():
+    logging.info("you are not yet assigning different k values")
+    angle_k = 20
+
+    all_beads = core_beads() + beads_2c() + beads_3c() + beads_4c()
+
+    angles = []
+    for triplet in itertools.combinations_with_replacement(
+        all_beads, 3
+    ):
+        # Atom1 type defines the centre atom.
+        acentered = triplet[1].angle_centered
+        if isinstance(acentered, int) or isinstance(acentered, float):
+            angles.append(
+                ThreeAngle(
+                    atom1_type=triplet[1].element_string,
+                    atom2_type=triplet[0].element_string,
+                    atom3_type=triplet[2].element_string,
+                    theta=acentered,
+                    angle_k=angle_k,
+                )
+            )
+        elif isinstance(acentered, tuple):
+            angles.append(
+                CheckedThreeAngle(
+                    atom1_type=triplet[1].element_string,
+                    atom2_type=triplet[0].element_string,
+                    atom3_type=triplet[2].element_string,
+                    cut_angle=acentered[2],
+                    min_angle=acentered[0],
+                    max_angle=acentered[1],
+                    angle_k=angle_k,
+                )
+            )
+
+    return tuple(angles)
