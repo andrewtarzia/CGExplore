5a8d862a369b8bfbbb93b9749a21624344fa1a2c
Andrew Tarzia <andrew.tarzia@gmail.com>
2023-01-10 22:36:48 +0100

    Add two bite angle parities.

--- src/analyse_all_cages.py
+++ src/analyse_all_cages.py
@@ -2646,69 +2646,149 @@ def parity_1(all_data, figure_output):
     plt.close()
 
 
-def not_opt_phase_space(all_data, figure_output):
-    color_map = topo_to_colormap()
-    tt_map = {j: i for i, j in enumerate(color_map)}
-    fig, axs = plt.subplots(
-        ncols=3,
-        nrows=3,
-        sharex=True,
-        sharey=True,
-        figsize=(16, 10),
-    )
-    flat_axs = axs.flatten()
+def parity_2(all_data, geom_data, figure_output):
+    tcmap = topo_to_colormap()
 
-    unopt_data = all_data[~all_data["optimised"]]
-    for tstr in tt_map:
-        ax = flat_axs[tt_map[tstr]]
-        topo_data = unopt_data[unopt_data["topology"] == tstr]
-
-        toff_data = topo_data[topo_data["torsions"] == "toff"]
-        c = "r"
-        s = 100
-        m = "s"
-        x = toff_data["clangle"]
-        y = toff_data["c2angle"]
-        ax.scatter(
-            x,
-            y,
-            c=c,
-            marker=m,
-            edgecolor="none",
-            s=s,
-            alpha=0.2,
+    c2labels = ("Pb_Ba_Ag",)
+    vmax = max_energy()
+    for tstr in tcmap:
+        fig, axs = plt.subplots(
+            nrows=1,
+            ncols=2,
+            sharex=True,
+            sharey=True,
+            figsize=(12, 5),
         )
+        flat_axs = axs.flatten()
+        topo_frame = all_data[all_data["topology"] == tstr]
+        for tors, ax in zip(("ton", "toff"), flat_axs):
+            tor_frame = topo_frame[topo_frame["torsions"] == tors]
 
-        ton_data = topo_data[topo_data["torsions"] == "ton"]
-        c = "k"
-        s = 50
-        m = "o"
-        x = ton_data["clangle"]
-        y = ton_data["c2angle"]
-        ax.scatter(
-            x,
-            y,
-            c=c,
-            marker=m,
-            edgecolor="none",
-            s=s,
-            alpha=0.2,
+            target_c2s = []
+            measured_c2 = []
+            energies = []
+            for i, row in tor_frame.iterrows():
+                c2angle = float(row["c2angle"])
+                target_angle = (c2angle / 2) + 90
+                name = str(row["index"])
+                energy = float(row["energy_per_bond"])
+                c2data = geom_data[name]["angles"]
+
+                for lbl in c2labels:
+                    if lbl in c2data:
+                        lbldata = c2data[lbl]
+                        for mc2 in lbldata:
+                            measured_c2.append(mc2)
+                            target_c2s.append(target_angle)
+                            energies.append(energy)
+            ax.scatter(
+                target_c2s,
+                measured_c2,
+                c=energies,
+                vmin=0,
+                vmax=vmax,
+                alpha=1.0,
+                edgecolor="k",
+                s=80,
+                cmap="Blues",
+            )
+
+            ax.plot((90, 180), (90, 180), c="k", lw=2, linestyle="--")
+
+            ax.tick_params(axis="both", which="major", labelsize=16)
+            ax.set_xlabel("target [deg]", fontsize=16)
+            ax.set_ylabel("observed [deg]", fontsize=16)
+            ax.set_title(
+                (
+                    f"{convert_topo_to_label(tstr)}: "
+                    f"{convert_torsion_to_label(tors, num=False)}"
+                ),
+                fontsize=16,
+            )
+
+        cbar_ax = fig.add_axes([1.01, 0.15, 0.02, 0.7])
+        cmap = mpl.cm.Blues
+        norm = mpl.colors.Normalize(vmin=0, vmax=vmax)
+        cbar = fig.colorbar(
+            mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+            cax=cbar_ax,
+            orientation="vertical",
         )
+        cbar.ax.tick_params(labelsize=16)
+        cbar.set_label("energy per bond formed [eV]", fontsize=16)
 
-        count_str = f"{len(toff_data)}/{len(ton_data)}"
-        ax.set_title(f"{tstr}: {count_str}", fontsize=16)
-        ax.tick_params(axis="both", which="major", labelsize=16)
-        # ax.set_xlabel("target 2c core size", fontsize=16)
-        ax.set_xlabel("target Lc angle", fontsize=16)
-        ax.set_ylabel("target 2c bite angle", fontsize=16)
+        fig.tight_layout()
+        fig.savefig(
+            os.path.join(figure_output, f"par_2_{tstr}.pdf"),
+            dpi=720,
+            bbox_inches="tight",
+        )
+        plt.close()
 
-    fig.tight_layout()
-    fig.savefig(
-        os.path.join(figure_output, "ps_notopt.pdf"),
-        dpi=720,
-        bbox_inches="tight",
-    )
-    plt.close()
+
+def parity_3(all_data, geom_data, figure_output):
+    tcmap = topo_to_colormap()
+
+    c2labels = ("Pb_Ba_Ag",)
+
+    for tstr in tcmap:
+        fig, axs = plt.subplots(
+            nrows=1,
+            ncols=2,
+            sharex=True,
+            sharey=True,
+            figsize=(12, 5),
+        )
+        flat_axs = axs.flatten()
+        topo_frame = all_data[all_data["topology"] == tstr]
+        for tors, ax in zip(("ton", "toff"), flat_axs):
+            tor_frame = topo_frame[topo_frame["torsions"] == tors]
+            target_c2s = []
+            measured_c2 = []
+            energies = []
+            for i, row in tor_frame.iterrows():
+                c2angle = float(row["c2angle"])
+                target_angle = (c2angle / 2) + 90
+                name = str(row["index"])
+                energy = float(row["energy_per_bond"])
+                c2data = geom_data[name]["angles"]
+
+                for lbl in c2labels:
+                    if lbl in c2data:
+                        lbldata = c2data[lbl]
+                        for mc2 in lbldata:
+                            measured_c2.append(mc2)
+                            target_c2s.append(target_angle)
+                            energies.append(energy)
+            ax.scatter(
+                [j - i for i, j in zip(measured_c2, target_c2s)],
+                energies,
+                c="gray",
+                alpha=1.0,
+                edgecolor="k",
+                s=30,
+            )
+
+            ax.axhline(y=0, c="k", lw=2, linestyle="--")
+
+            ax.tick_params(axis="both", which="major", labelsize=16)
+            ax.set_xlabel("target - observed [deg]", fontsize=16)
+            ax.set_ylabel("energy per bond formed [eV]", fontsize=16)
+            ax.set_title(
+                (
+                    f"{convert_topo_to_label(tstr)}: "
+                    f"{convert_torsion_to_label(tors, num=False)}"
+                ),
+                fontsize=16,
+            )
+
+        fig.tight_layout()
+        fig.savefig(
+            os.path.join(figure_output, f"par_3_{tstr}.pdf"),
+            dpi=720,
+            bbox_inches="tight",
+        )
+        plt.close()
 
 
 def bite_angle_relationship(all_data, figure_output):
