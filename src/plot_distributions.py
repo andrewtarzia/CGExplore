ea66f9acb75de9950643be277501abac90209750
Andrew Tarzia <andrew.tarzia@gmail.com>
2023-01-11 17:38:33 +0100

    Update geom distributions.

--- src/plot_distributions.py
+++ src/plot_distributions.py
@@ -15,9 +15,13 @@ import stk
 import stko
 import json
 import logging
+import pandas as pd
 import matplotlib.pyplot as plt
+
+# import matplotlib as mpl
 from env_set import cages
 
+from utilities import convert_pyramid_angle
 from analysis_utilities import (
     write_out_mapping,
     data_to_array,
@@ -26,11 +30,12 @@ from analysis_utilities import (
     topology_labels,
     target_shapes,
     topo_to_colormap,
-    max_energy,
+    # max_energy,
 )
 
 
 def identity_distributions(all_data, figure_output):
+    logging.info("running identity_distributions")
 
     fig, ax = plt.subplots(figsize=(16, 5))
 
@@ -69,10 +74,20 @@ def identity_distributions(all_data, figure_output):
 
 
 def geom_distributions(all_data, geom_data, figure_output):
+    logging.info("running geom_distributions")
+    # vmax = max_energy()
+
     comparisons = {
+        "torsions": {
+            "measure": "dihedrals",
+            "xlabel": "torsion [deg]",
+            "column": None,
+            "label_options": ("Pb_Ba_Ba_Pb",),
+        },
         "clangle": {
             "measure": "angles",
-            "xlabel": "angle [deg]",
+            "xlabel": "CL angle [deg]",
+            "column": "clangle",
             "label_options": (
                 "Pb_Pd_Pb",
                 "Pb_C_Pb",
@@ -82,90 +97,177 @@ def geom_distributions(all_data, geom_data, figure_output):
         },
         "clsigma": {
             "measure": "bonds",
-            "xlabel": "length [A]",
+            "xlabel": "CL bond length [A]",
+            "column": "clsigma",
             "label_options": ("Pd_Pb", "C_Pb"),
         },
         "c2angle": {
             "measure": "angles",
-            "xlabel": "angle [deg]",
-            "label_options": ("Ba_Ag_Ba", "Pb_Ba_Ag"),
+            "xlabel": "bonder angle [deg]",
+            "column": "c2angle",
+            "label_options": ("Pb_Ba_Ag",),
+        },
+        "c2backbone": {
+            "measure": "angles",
+            "xlabel": "backbone angle [deg]",
+            "column": None,
+            "label_options": ("Ba_Ag_Ba",),
         },
         "c2sigma": {
             "measure": "bonds",
-            "xlabel": "length [A]",
-            "label_options": ("Pb_Ba", "Ba_Ag"),
+            "xlabel": "C2 backbone length [A]",
+            "column": "c2sigma",
+            "label_options": ("Ba_Ag",),
         },
-        "torsions": {
-            "measure": "dihedrals",
-            "xlabel": "torsion [deg]",
-            "label_options": ("Pb_Ba_Ba_Pb",),
+        "c2bonder": {
+            "measure": "bonds",
+            "xlabel": "C2 bonder length [A]",
+            "column": None,
+            "label_options": ("Pb_Ba",),
         },
     }
+
+    tcmap = topo_to_colormap()
+    tcpos = {tstr: i for i, tstr in enumerate(tcmap)}
 
     for comp in comparisons:
         cdict = comparisons[comp]
         color_map = topo_to_colormap()
+        column = cdict["column"]
 
-        for tors in ("ton", "toff"):
+        fig, axs = plt.subplots(ncols=2, sharey=True, figsize=(16, 5))
+        for ax, tors in zip(axs, ("ton", "toff")):
+            tor_frame = all_data[all_data["torsions"] == tors]
             for i, tstr in enumerate(color_map):
-                fig, ax = plt.subplots(figsize=(8, 5))
-                topo_frame = all_data[all_data["topology"] == tstr]
-                # c_lines = list(set(topo_frame[comp]))
-                tor_frame = topo_frame[topo_frame["torsions"] == tors]
-                tor_names = list(tor_frame["index"])
-                tor_energies = list(tor_frame["energy"])
+                topo_frame = tor_frame[tor_frame["topology"] == tstr]
 
                 values = []
-                highe_values = []
-                for i, j in zip(tor_names, tor_energies):
-                    gdata = geom_data[i][cdict["measure"]]
+                # energies = []
+                for i, row in topo_frame.iterrows():
+                    name = str(row["index"])
+                    # energy = float(row["energy_per_bond"])
+                    if column is None:
+                        target = 0
+                        target_oppos = None
+                    else:
+                        target = float(row[column])
+                        if "4C1" in name and column == "clangle":
+                            target_oppos = convert_pyramid_angle(target)
+                        else:
+                            target_oppos = None
+
+                    # Ignore torsions for target bite-angles near 180.
+                    if comp == "torsions":
+                        tba = float(row["target_bite_angle"])
+                        if tba == 180:
+                            continue
+
+                    obsdata = geom_data[name][cdict["measure"]]
                     for lbl in cdict["label_options"]:
-                        if lbl in gdata:
-                            lbldata = gdata[lbl]
-                            if j < max_energy():
-                                values.extend(lbldata)
-                            else:
-                                highe_values.extend(lbldata)
+                        if lbl in obsdata:
+                            lbldata = obsdata[lbl]
+                            for observed in lbldata:
+                                if pd.isna(observed):
+                                    continue
 
-                ax.hist(
-                    x=values,
-                    bins=50,
-                    density=False,
-                    histtype="step",
-                    color="r",
-                    lw=2,
-                    label=f"E < {max_energy()}",
+                                # For some comparisons, need to use
+                                # matching rules.
+                                if comp == "clsigma":
+                                    new_target = (target + 1) / 2
+                                    value = observed - new_target
+                                elif comp == "c2sigma":
+                                    new_target = (target + 1) / 2
+                                    value = observed - new_target
+                                else:
+                                    value = observed - target
+
+                                # If it is possible, this checks if it
+                                # matches the opposite angle in
+                                # square-pyramid instead.
+                                if target_oppos is not None:
+                                    ovalue = observed - target_oppos
+                                    value = min(
+                                        (abs(ovalue), abs(value))
+                                    )
+
+                                values.append(value)
+                                # energies.append(energy)
+
+                xpos = tcpos[tstr]
+
+                ax.scatter(
+                    [xpos for i in values],
+                    [i for i in values],
+                    c="gray",
+                    # c=[i for i in energies],
+                    edgecolor="none",
+                    s=20,
+                    alpha=0.2,
+                    rasterized=True,
+                    # vmin=0,
+                    # vmax=vmax,
+                    # cmap="Blues",
                 )
-                ax.hist(
-                    x=highe_values,
-                    bins=50,
-                    density=False,
-                    histtype="step",
-                    color="gray",
-                    lw=2,
-                    alpha=1.0,
-                    label=f"E > {max_energy()}",
-                    linestyle="-",
+                if column is not None:
+                    ax.axhline(y=0, lw=2, c="k", linestyle="--")
+
+                parts = ax.violinplot(
+                    [i for i in values],
+                    [xpos],
+                    # points=200,
+                    vert=True,
+                    widths=0.8,
+                    showmeans=False,
+                    showextrema=False,
+                    showmedians=False,
+                    # bw_method=0.5,
                 )
 
-                ax.tick_params(axis="both", which="major", labelsize=16)
-                ax.set_xlabel(cdict["xlabel"], fontsize=16)
-                ax.set_ylabel("count", fontsize=16)
-                ax.set_title(tstr, fontsize=16)
-                ax.legend(fontsize=16)
+                for pc in parts["bodies"]:
+                    pc.set_facecolor("gray")
+                    pc.set_edgecolor("none")
+                    pc.set_alpha(0.3)
 
-                fig.tight_layout()
-                fig.savefig(
-                    os.path.join(
-                        figure_output, f"gd_{tors}_{comp}_{tstr}.pdf"
-                    ),
-                    dpi=720,
-                    bbox_inches="tight",
-                )
-                plt.close()
+            ax.tick_params(axis="both", which="major", labelsize=16)
+            ax.set_title(
+                (
+                    f'{cdict["xlabel"]}: '
+                    f"{convert_torsion_to_label(tors,num=False)}"
+                ),
+                fontsize=16,
+            )
+            if column is None:
+                ax.set_ylabel("observed", fontsize=16)
+            else:
+                ax.set_ylabel("observed - target", fontsize=16)
+            ax.set_xticks([tcpos[i] for i in tcpos])
+            ax.set_xticklabels(
+                [convert_topo_to_label(i) for i in tcpos],
+                rotation=45,
+            )
+
+        # cbar_ax = fig.add_axes([1.01, 0.15, 0.02, 0.7])
+        # cmap = mpl.cm.Blues
+        # norm = mpl.colors.Normalize(vmin=0, vmax=vmax)
+        # cbar = fig.colorbar(
+        #     mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+        #     cax=cbar_ax,
+        #     orientation="vertical",
+        # )
+        # cbar.ax.tick_params(labelsize=16)
+        # cbar.set_label("energy per bond formed [eV]", fontsize=16)
+
+        fig.tight_layout()
+        fig.savefig(
+            os.path.join(figure_output, f"gd_{comp}.pdf"),
+            dpi=720,
+            bbox_inches="tight",
+        )
+        plt.close()
 
 
 def rmsd_distributions(all_data, calculation_dir, figure_output):
+    logging.info("running rmsd_distributions")
 
     tcmap = topo_to_colormap()
 
@@ -260,7 +362,7 @@ def rmsd_distributions(all_data, calculation_dir, figure_output):
             showmeans=False,
             showextrema=False,
             showmedians=False,
-            bw_method=0.5,
+            # bw_method=0.5,
         )
 
         for pc in parts["bodies"]:
@@ -287,7 +389,7 @@ def rmsd_distributions(all_data, calculation_dir, figure_output):
             showmeans=False,
             showextrema=False,
             showmedians=False,
-            bw_method=0.5,
+            # bw_method=0.5,
         )
 
         for pc in parts["bodies"]:
@@ -338,6 +440,8 @@ def rmsd_distributions(all_data, calculation_dir, figure_output):
 
 
 def single_value_distributions(all_data, figure_output):
+    logging.info("running single_value_distributions")
+
     to_plot = {
         "energy": {"xtitle": "energy", "xlim": (0, 100)},
         "gnorm": {"xtitle": "gnorm", "xlim": (0, 0.0005)},
@@ -390,7 +494,7 @@ def single_value_distributions(all_data, figure_output):
                 showmeans=False,
                 showextrema=False,
                 showmedians=False,
-                bw_method=0.5,
+                # bw_method=0.5,
             )
 
             for pc in parts["bodies"]:
@@ -437,6 +541,8 @@ def single_value_distributions(all_data, figure_output):
 
 
 def shape_vector_distributions(all_data, figure_output):
+    logging.info("running shape_vector_distributions")
+
     present_shape_values = target_shapes()
     num_cols = 4
     num_rows = 3
