f7674bb0d8e3b8afb1bf0be4107665d610f1f116
Andrew Tarzia <andrew.tarzia@gmail.com>
2023-02-10 17:51:41 +0100

    Bug fix.

--- src/analysis_utilities.py
+++ src/analysis_utilities.py
@@ -524,7 +524,7 @@ def data_to_array(json_files, output_dir):
             row["min_b2b_distance"] = res_dict["min_b2b_distance"]
             row["radius_gyration"] = res_dict["radius_gyration"]
             row["max_diameter"] = res_dict["max_diameter"]
-            row["rg/md"] = res_dict["rg/md"]
+            row["rg_md"] = res_dict["rg/md"]
             row["flexibility_measure"] = res_dict["flexibility_measure"]
 
             bond_data = res_dict["bond_data"]
@@ -759,7 +759,7 @@ def write_out_mapping(all_data):
         "min_b2b_distance",
         "radius_gyration",
         "max_diameter",
-        "rg/md",
+        "rg_md",
         "flexibility_measure",
         "CustomNonbondedForce_kjmol",
         "PeriodicTorsionForce_kjmol",
