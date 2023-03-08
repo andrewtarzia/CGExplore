c9965a561e32b993a412c30c86fdee99717bcad9
Andrew Tarzia <andrew.tarzia@gmail.com>
2023-03-08 22:51:09 +0100

    Use CG model for all.

--- src/cage_construction/topologies.py
+++ src/cage_construction/topologies.py
@@ -15,18 +15,18 @@ import stk
 def cage_topology_options(fg_set):
     if fg_set == "2p3":
         topologies = {
-            "2P3": stk.cage.TwoPlusThree,
-            "4P6": stk.cage.FourPlusSix,
-            "4P62": stk.cage.FourPlusSix2,
-            "6P9": stk.cage.SixPlusNine,
-            "8P12": stk.cage.EightPlusTwelve,
+            "2P3": CGTwoPlusThree,
+            "4P6": CGFourPlusSix,
+            "4P62": CGFourPlusSix2,
+            "6P9": CGSixPlusNine,
+            "8P12": CGEightPlusTwelve,
         }
     if fg_set == "2p4":
         topologies = {
-            "2P4": stk.cage.M2L4Lantern,
-            "3P6": stk.cage.M3L6,
-            "4P8": stk.cage.M4L8,
-            "6P12": stk.cage.M6L12Cube,
+            "2P4": CGM2L4Lantern,
+            "3P6": CGM3L6,
+            "4P8": CGM6L12Cube,
+            "6P12": CGM6L12Cube,
             "8P16": stk.cage.EightPlusSixteen,
             "12P24": CGM12L24,
         }
@@ -41,6 +41,51 @@ def cage_topology_options(fg_set):
         }
 
     return topologies
+
+
+class CGTwoPlusThree(stk.cage.TwoPlusThree):
+    def _get_scale(self, building_block_vertices):
+        return 5
+
+
+class CGFourPlusSix(stk.cage.FourPlusSix):
+    def _get_scale(self, building_block_vertices):
+        return 10
+
+
+class CGFourPlusSix2(stk.cage.FourPlusSix2):
+    def _get_scale(self, building_block_vertices):
+        return 10
+
+
+class CGSixPlusNine(stk.cage.SixPlusNine):
+    def _get_scale(self, building_block_vertices):
+        return 15
+
+
+class CGEightPlusTwelve(stk.cage.EightPlusTwelve):
+    def _get_scale(self, building_block_vertices):
+        return 15
+
+
+class CGM2L4Lantern(stk.cage.M2L4Lantern):
+    def _get_scale(self, building_block_vertices):
+        return 5
+
+
+class CGM3L6(stk.cage.M3L6):
+    def _get_scale(self, building_block_vertices):
+        return 10
+
+
+class CGM4L8(stk.cage.M4L8):
+    def _get_scale(self, building_block_vertices):
+        return 10
+
+
+class CGM6L12Cube(stk.cage.M6L12Cube):
+    def _get_scale(self, building_block_vertices):
+        return 15
 
 
 class CGM12L24(stk.cage.M12L24):
