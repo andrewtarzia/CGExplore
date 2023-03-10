52c3d57fb420f84fc1aad90d8137637e57d0c246
Andrew Tarzia <andrew.tarzia@gmail.com>
2023-03-10 18:12:14 +0100

    Update and modify CG topologies for cleaner initial structures.

--- src/cage_construction/topologies.py
+++ src/cage_construction/topologies.py
@@ -15,18 +15,18 @@ import stk
 def cage_topology_options(fg_set):
     if fg_set == "2p3":
         topologies = {
-            "2P3": CGTwoPlusThree,
-            "4P6": CGFourPlusSix,
-            "4P62": CGFourPlusSix2,
-            "6P9": CGSixPlusNine,
-            "8P12": CGEightPlusTwelve,
+            "2P3": stk.cage.TwoPlusThree,
+            "4P6": stk.cage.FourPlusSix,
+            "4P62": stk.cage.FourPlusSix2,
+            "6P9": stk.cage.SixPlusNine,
+            "8P12": stk.cage.EightPlusTwelve,
         }
     if fg_set == "2p4":
         topologies = {
-            "2P4": CGM2L4Lantern,
-            "3P6": CGM3L6,
-            "4P8": CGM6L12Cube,
-            "6P12": CGM6L12Cube,
+            "2P4": stk.cage.M2L4Lantern,
+            "3P6": stk.cage.M3L6,
+            "4P8": CGM4L8,
+            "6P12": stk.cage.M6L12Cube,
             "8P16": stk.cage.EightPlusSixteen,
             "12P24": CGM12L24,
         }
@@ -43,54 +43,136 @@ def cage_topology_options(fg_set):
     return topologies
 
 
-class CGTwoPlusThree(stk.cage.TwoPlusThree):
-    def _get_scale(self, building_block_vertices):
-        return 5
-
-
-class CGFourPlusSix(stk.cage.FourPlusSix):
-    def _get_scale(self, building_block_vertices):
-        return 10
-
-
-class CGFourPlusSix2(stk.cage.FourPlusSix2):
-    def _get_scale(self, building_block_vertices):
-        return 10
-
-
-class CGSixPlusNine(stk.cage.SixPlusNine):
-    def _get_scale(self, building_block_vertices):
-        return 15
-
-
-class CGEightPlusTwelve(stk.cage.EightPlusTwelve):
-    def _get_scale(self, building_block_vertices):
-        return 15
-
-
-class CGM2L4Lantern(stk.cage.M2L4Lantern):
-    def _get_scale(self, building_block_vertices):
-        return 5
-
-
-class CGM3L6(stk.cage.M3L6):
-    def _get_scale(self, building_block_vertices):
-        return 10
-
-
 class CGM4L8(stk.cage.M4L8):
-    def _get_scale(self, building_block_vertices):
-        return 10
 
+    _vertex_prototypes = (
+        stk.cage.NonLinearVertex(0, [2, 0, 0]),
+        stk.cage.NonLinearVertex(1, [0, 2, 0]),
+        stk.cage.NonLinearVertex(2, [-2, 0, 0]),
+        stk.cage.NonLinearVertex(3, [0, -2, 0]),
+        stk.cage.LinearVertex(4, [1, 1, 0.5], False),
+        stk.cage.LinearVertex(5, [1, 1, -0.5], False),
+        stk.cage.LinearVertex(6, [1, -1, 0.5], False),
+        stk.cage.LinearVertex(7, [1, -1, -0.5], False),
+        stk.cage.LinearVertex(8, [-1, -1, 0.5], False),
+        stk.cage.LinearVertex(9, [-1, -1, -0.5], False),
+        stk.cage.LinearVertex(10, [-1, 1, 0.5], False),
+        stk.cage.LinearVertex(11, [-1, 1, -0.5], False),
+    )
 
-class CGM6L12Cube(stk.cage.M6L12Cube):
-    def _get_scale(self, building_block_vertices):
-        return 15
+    _edge_prototypes = (
+        stk.Edge(0, _vertex_prototypes[0], _vertex_prototypes[4]),
+        stk.Edge(1, _vertex_prototypes[0], _vertex_prototypes[5]),
+        stk.Edge(2, _vertex_prototypes[0], _vertex_prototypes[6]),
+        stk.Edge(3, _vertex_prototypes[0], _vertex_prototypes[7]),
+        stk.Edge(4, _vertex_prototypes[1], _vertex_prototypes[4]),
+        stk.Edge(5, _vertex_prototypes[1], _vertex_prototypes[5]),
+        stk.Edge(6, _vertex_prototypes[1], _vertex_prototypes[10]),
+        stk.Edge(7, _vertex_prototypes[1], _vertex_prototypes[11]),
+        stk.Edge(8, _vertex_prototypes[2], _vertex_prototypes[10]),
+        stk.Edge(9, _vertex_prototypes[2], _vertex_prototypes[11]),
+        stk.Edge(10, _vertex_prototypes[2], _vertex_prototypes[8]),
+        stk.Edge(11, _vertex_prototypes[2], _vertex_prototypes[9]),
+        stk.Edge(12, _vertex_prototypes[3], _vertex_prototypes[8]),
+        stk.Edge(13, _vertex_prototypes[3], _vertex_prototypes[9]),
+        stk.Edge(14, _vertex_prototypes[3], _vertex_prototypes[6]),
+        stk.Edge(15, _vertex_prototypes[3], _vertex_prototypes[7]),
+    )
 
 
 class CGM12L24(stk.cage.M12L24):
+    _vertex_prototypes = (
+        stk.cage.NonLinearVertex(0, [1.25, 0, 0]),
+        stk.cage.NonLinearVertex(1, [-1.25, 0, 0]),
+        stk.cage.NonLinearVertex(2, [0, 1.25, 0]),
+        stk.cage.NonLinearVertex(3, [0, -1.25, 0]),
+        stk.cage.NonLinearVertex(4, [0.625, 0.625, 0.88]),
+        stk.cage.NonLinearVertex(5, [0.625, -0.625, 0.88]),
+        stk.cage.NonLinearVertex(6, [-0.625, 0.625, 0.88]),
+        stk.cage.NonLinearVertex(7, [-0.625, -0.625, 0.88]),
+        stk.cage.NonLinearVertex(8, [0.625, 0.625, -0.88]),
+        stk.cage.NonLinearVertex(9, [0.625, -0.625, -0.88]),
+        stk.cage.NonLinearVertex(10, [-0.625, 0.625, -0.88]),
+        stk.cage.NonLinearVertex(11, [-0.625, -0.625, -0.88]),
+        stk.cage.LinearVertex(12, [0.9, 0.31, 0.31], False),
+        stk.cage.LinearVertex(13, [0.9, 0.31, -0.31], False),
+        stk.cage.LinearVertex(14, [0.9, -0.31, 0.31], False),
+        stk.cage.LinearVertex(15, [0.9, -0.31, -0.31], False),
+        stk.cage.LinearVertex(16, [-0.9, 0.31, 0.31], False),
+        stk.cage.LinearVertex(17, [-0.9, 0.31, -0.31], False),
+        stk.cage.LinearVertex(18, [-0.9, -0.31, 0.31], False),
+        stk.cage.LinearVertex(19, [-0.9, -0.31, -0.31], False),
+        stk.cage.LinearVertex(20, [0.31, 0.9, 0.31], False),
+        stk.cage.LinearVertex(21, [0.31, 0.9, -0.31], False),
+        stk.cage.LinearVertex(22, [-0.31, 0.9, 0.31], False),
+        stk.cage.LinearVertex(23, [-0.31, 0.9, -0.31], False),
+        stk.cage.LinearVertex(24, [0.31, -0.9, 0.31], False),
+        stk.cage.LinearVertex(25, [0.31, -0.9, -0.31], False),
+        stk.cage.LinearVertex(26, [-0.31, -0.9, 0.31], False),
+        stk.cage.LinearVertex(27, [-0.31, -0.9, -0.31], False),
+        stk.cage.LinearVertex(28, [0.58, 0, 0.82], False),
+        stk.cage.LinearVertex(29, [-0.58, 0, 0.82], False),
+        stk.cage.LinearVertex(30, [0, 0.58, 0.82], False),
+        stk.cage.LinearVertex(31, [0, -0.58, 0.82], False),
+        stk.cage.LinearVertex(32, [0.58, 0, -0.82], False),
+        stk.cage.LinearVertex(33, [-0.58, 0, -0.82], False),
+        stk.cage.LinearVertex(34, [0, 0.58, -0.82], False),
+        stk.cage.LinearVertex(35, [0, -0.58, -0.82], False),
+    )
+
+    _edge_prototypes = (
+        stk.Edge(0, _vertex_prototypes[0], _vertex_prototypes[12]),
+        stk.Edge(1, _vertex_prototypes[0], _vertex_prototypes[13]),
+        stk.Edge(2, _vertex_prototypes[0], _vertex_prototypes[14]),
+        stk.Edge(3, _vertex_prototypes[0], _vertex_prototypes[15]),
+        stk.Edge(4, _vertex_prototypes[1], _vertex_prototypes[16]),
+        stk.Edge(5, _vertex_prototypes[1], _vertex_prototypes[17]),
+        stk.Edge(6, _vertex_prototypes[1], _vertex_prototypes[18]),
+        stk.Edge(7, _vertex_prototypes[1], _vertex_prototypes[19]),
+        stk.Edge(8, _vertex_prototypes[2], _vertex_prototypes[20]),
+        stk.Edge(9, _vertex_prototypes[2], _vertex_prototypes[21]),
+        stk.Edge(10, _vertex_prototypes[2], _vertex_prototypes[22]),
+        stk.Edge(11, _vertex_prototypes[2], _vertex_prototypes[23]),
+        stk.Edge(12, _vertex_prototypes[3], _vertex_prototypes[24]),
+        stk.Edge(13, _vertex_prototypes[3], _vertex_prototypes[25]),
+        stk.Edge(14, _vertex_prototypes[3], _vertex_prototypes[26]),
+        stk.Edge(15, _vertex_prototypes[3], _vertex_prototypes[27]),
+        stk.Edge(16, _vertex_prototypes[4], _vertex_prototypes[28]),
+        stk.Edge(17, _vertex_prototypes[4], _vertex_prototypes[30]),
+        stk.Edge(18, _vertex_prototypes[4], _vertex_prototypes[12]),
+        stk.Edge(19, _vertex_prototypes[4], _vertex_prototypes[20]),
+        stk.Edge(20, _vertex_prototypes[5], _vertex_prototypes[14]),
+        stk.Edge(21, _vertex_prototypes[5], _vertex_prototypes[24]),
+        stk.Edge(22, _vertex_prototypes[5], _vertex_prototypes[28]),
+        stk.Edge(23, _vertex_prototypes[5], _vertex_prototypes[31]),
+        stk.Edge(24, _vertex_prototypes[6], _vertex_prototypes[16]),
+        stk.Edge(25, _vertex_prototypes[6], _vertex_prototypes[29]),
+        stk.Edge(26, _vertex_prototypes[6], _vertex_prototypes[30]),
+        stk.Edge(27, _vertex_prototypes[6], _vertex_prototypes[22]),
+        stk.Edge(28, _vertex_prototypes[7], _vertex_prototypes[18]),
+        stk.Edge(29, _vertex_prototypes[7], _vertex_prototypes[26]),
+        stk.Edge(30, _vertex_prototypes[7], _vertex_prototypes[31]),
+        stk.Edge(31, _vertex_prototypes[7], _vertex_prototypes[29]),
+        stk.Edge(32, _vertex_prototypes[8], _vertex_prototypes[13]),
+        stk.Edge(33, _vertex_prototypes[8], _vertex_prototypes[32]),
+        stk.Edge(34, _vertex_prototypes[8], _vertex_prototypes[34]),
+        stk.Edge(35, _vertex_prototypes[8], _vertex_prototypes[21]),
+        stk.Edge(36, _vertex_prototypes[9], _vertex_prototypes[15]),
+        stk.Edge(37, _vertex_prototypes[9], _vertex_prototypes[32]),
+        stk.Edge(38, _vertex_prototypes[9], _vertex_prototypes[35]),
+        stk.Edge(39, _vertex_prototypes[9], _vertex_prototypes[25]),
+        stk.Edge(40, _vertex_prototypes[10], _vertex_prototypes[17]),
+        stk.Edge(41, _vertex_prototypes[10], _vertex_prototypes[23]),
+        stk.Edge(42, _vertex_prototypes[10], _vertex_prototypes[34]),
+        stk.Edge(43, _vertex_prototypes[10], _vertex_prototypes[33]),
+        stk.Edge(44, _vertex_prototypes[11], _vertex_prototypes[19]),
+        stk.Edge(45, _vertex_prototypes[11], _vertex_prototypes[33]),
+        stk.Edge(46, _vertex_prototypes[11], _vertex_prototypes[27]),
+        stk.Edge(47, _vertex_prototypes[11], _vertex_prototypes[35]),
+    )
+
     def _get_scale(self, building_block_vertices):
-        return 15
+        return 10
 
     def get_vertex_alignments(self):
         return self._vertex_alignments
