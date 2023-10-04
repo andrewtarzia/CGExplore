31c9fcecfa9a018c015e922bb4e01b0a66361e87
andrewtarzia <andrew.tarzia@gmail.com>
2023-10-04 17:30:21 +0200

    Write tests for assigned system based on xmls.

---
+++ tests/assigned/test_system_xml_writer.py
@@ -0,0 +1,59 @@
+import os
+import pathlib
+
+from cgexplore.errors import ForcefieldUnitError
+
+
+def test_system_xml_writer(molecule):
+    """
+    Test methods toward :meth:`.ForceField.get_xml_string`.
+
+    Parameters:
+
+        molecule:
+            The molecule.
+
+    Returns:
+
+        None : :class:`NoneType`
+
+    """
+
+    try:
+        syst_xml_file = pathlib.Path(
+            os.path.dirname(os.path.realpath(__file__))
+        ) / (
+            f"{molecule.name}_{molecule.force_field.get_prefix()}_"
+            f"{molecule.name}_syst.xml"
+        )
+        topo_xml_file = pathlib.Path(
+            os.path.dirname(os.path.realpath(__file__))
+        ) / (
+            f"{molecule.name}_{molecule.force_field.get_prefix()}_"
+            f"{molecule.name}_topo.xml"
+        )
+        saved_syst_xml_file = pathlib.Path(
+            os.path.dirname(os.path.realpath(__file__))
+        ) / (
+            f"{molecule.name}_{molecule.force_field.get_prefix()}_"
+            f"{molecule.name}_syst_saved.xml"
+        )
+
+        assigned_system = molecule.force_field.assign_terms(
+            molecule=molecule.molecule,
+            output_dir=pathlib.Path(
+                os.path.dirname(os.path.realpath(__file__))
+            ),
+            name=molecule.name,
+        )
+        assigned_system.get_openmm_system()
+        with open(syst_xml_file, "r") as f:
+            xml_string = f.read()
+        with open(saved_syst_xml_file, "r") as f:
+            test_xml_string = f.read()
+        print(xml_string)
+        assert xml_string == test_xml_string
+        os.system(f"rm {syst_xml_file}")
+        os.system(f"rm {topo_xml_file}")
+    except ForcefieldUnitError:
+        assert molecule.num_forcefields == 0
