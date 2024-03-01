5dda0737247bfd2fdd942ad31365b27c4f0633dd
andrewtarzia <andrew.tarzia@gmail.com>
2024-03-01 20:48:20 +0100

    Add a host-guest viz option.

--- src/cgexplore/_internal/utilities/visualisation.py
+++ src/cgexplore/_internal/utilities/visualisation.py
@@ -58,13 +58,12 @@ class Pymol:
             max_max_diam = max((max_diam, max_max_diam))
         return f"zoom center, {max_max_diam/zoom_scale}"
 
-    def _write_pymol_script(  # noqa: PLR0913, PLR0912
+    def _write_pymol_script(
         self,
         structure_files: list,
         structure_colours: list | None,
         pml_file: pathlib.Path,
         orient_atoms: str | None,
-        big_colour: str | None,
     ) -> None:
         if self._settings["zoom_string"] == "custom":
             zoom_string = self._get_zoom_string(
@@ -87,11 +86,6 @@ class Pymol:
         else:
             orient_string = ""
 
-        if big_colour is None:
-            big_colour_string = ""
-        else:
-            big_colour_string = f"orient (name {orient_atoms}*)"
-
         lstring = ""
         cstring = ""
         lnames = []
@@ -108,7 +102,6 @@ class Pymol:
         string = (
             f"{lstring}\n"
             f"{cstring}\n"
-            f"{big_colour_string}\n"
             f"set grid_mode, {self._settings['grid_mode']}\n"
             "as sticks\n"
             f"set stick_radius, {self._settings['stick_rad']}\n"
@@ -133,7 +126,6 @@ class Pymol:
         structure_files: list,
         structure_colours: list | None = None,
         orient_atoms: str | None = None,
-        big_colour: str | None = None,
     ) -> None:
         """Run pymol to visualise a molecule."""
         pml_file = self._output_dir / f"{self._file_prefix}.pml"
@@ -142,7 +134,138 @@ class Pymol:
             structure_colours=structure_colours,
             pml_file=pml_file,
             orient_atoms=orient_atoms,
-            big_colour=big_colour,
+        )
+        _ = sp.run(
+            [f"{self._pymol}", "-c", "-q", f"{pml_file}"],  # noqa: S603
+            stdin=sp.PIPE,
+            capture_output=True,
+            check=True,
+        )
+
+    def _write_host_guest_pymol_script(  # noqa: C901, PLR0913, PLR0912
+        self,
+        structure_files: list[pathlib.Path],
+        host_atoms: list[str],
+        host_transperancy: float,
+        radii: dict[str, float],
+        epsilons: dict[str, float],
+        epsilon_min: float,
+        epsilon_max: float,
+        epsilon_palette: str,
+        structure_colours: list | None,
+        pml_file: pathlib.Path,
+        orient_atoms: str | None,
+    ) -> None:
+        if self._settings["zoom_string"] == "custom":
+            zoom_string = self._get_zoom_string(
+                structure_files=structure_files,
+                zoom_scale=self._settings["zoom_scale"],
+            )
+        else:
+            zoom_string = self._settings["zoom_string"]
+
+        if structure_colours is None:
+            show_colours = ["#000000" for i in structure_files]
+        else:
+            show_colours = structure_colours
+
+        if self._settings["orient"]:
+            if orient_atoms is None:
+                orient_string = "orient"
+            else:
+                orient_string = f"orient (name {orient_atoms}*)"
+        else:
+            orient_string = ""
+
+        lstring = ""
+        cstring = ""
+        lnames = []
+        for sf, col in zip(structure_files, show_colours, strict=True):
+            lstring += f"load {sf}\n"
+            lname = str(sf.name).replace(".mol", "")
+            lnames.append(lname)
+            tcol = col.replace("#", "0x")
+            if structure_colours is None:
+                cstring += "color orange, (name C*)\n"
+            else:
+                cstring += f"color {tcol}, {lname}\n"
+
+        selection_string = ""
+        for i, element in enumerate(host_atoms):
+            if i == 0:
+                selection_string += f"select cage, name {element}"
+            else:
+                selection_string += f" | name {element}"
+
+        radii_string = ""
+        for element in radii:
+            r = radii[element]
+            radii_string += f"alter (name {element}),vdw={r}\n"
+        transperancy_string = (
+            f"set sphere_transparency, {host_transperancy}, cage"
+        )
+
+        epsilon_string = ""
+        for element in epsilons:
+            b = epsilons[element]
+            if element in host_atoms:
+                continue
+            epsilon_string += f"alter (name {element}),b={b}\n"
+        spectrum_string = (
+            f"spectrum b, {epsilon_palette}, not cage, {epsilon_min}, "
+            f"{epsilon_max}\n"
+        )
+
+        string = (
+            f"{lstring}\n"
+            f"{cstring}\n"
+            f"set grid_mode, {self._settings['grid_mode']}\n"
+            "as spheres\n"
+            f"{selection_string}\n"
+            f"{radii_string}\n"
+            f"{epsilon_string}\n"
+            "rebuild\n"
+            f"{orient_string}\n"
+            f"{transperancy_string}\n"
+            f"{spectrum_string}\n"
+            f"{zoom_string}\n"
+            "show lines\n"
+            "bg_color white\n"
+            f"ray {self._settings['rayx']}, {self._settings['rayy']}\n"
+            f"png {self._output_dir / self._file_prefix}.png\n"
+            "quit\n"
+        )
+
+        with open(pml_file, "w") as f:
+            f.write(string)
+
+    def visualise_host_guest(  # noqa: PLR0913
+        self,
+        structure_file: pathlib.Path,
+        host_atoms: list[str],
+        host_transperancy: float,
+        radii: dict[str, float],
+        epsilons: dict[str, float],
+        epsilon_min: float,
+        epsilon_max: float,
+        epsilon_palette: str,
+        structure_colour: str | None = None,
+        orient_atoms: str | None = None,
+    ) -> None:
+        """Run pymol to visualise a molecule."""
+        pml_file = self._output_dir / f"{self._file_prefix}.pml"
+        self._write_host_guest_pymol_script(
+            structure_files=[structure_file],
+            structure_colours=structure_colour,
+            host_atoms=host_atoms,
+            host_transperancy=host_transperancy,
+            radii=radii,
+            epsilons=epsilons,
+            pml_file=pml_file,
+            orient_atoms=orient_atoms,
+            epsilon_min=epsilon_min,
+            epsilon_max=epsilon_max,
+            epsilon_palette=epsilon_palette,
         )
         _ = sp.run(
             [f"{self._pymol}", "-c", "-q", f"{pml_file}"],  # noqa: S603
