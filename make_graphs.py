b49eec7fa4503d4bcfab07b66513d049c7924b71
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-10-10 18:46:50 +0100

    Update graph test script.

--- make_graphs.py
+++ make_graphs.py
@@ -16,9 +16,51 @@ def main() -> None:
         3: stk.BuildingBlock("BrCC(Br)CCBr", (stk.BromoFactory(),)),
         4: stk.BuildingBlock("BrCC(Br)CCC(Br)CCBr", (stk.BromoFactory(),)),
     }
-    multipliers = range(1, 11)
+
+    # Three typers.
+    multipliers = range(1, 5)
+    three_type_stoichiometries = tuple(
+        (i, j, k) for i, j, k in it.product((1, 2, 3, 4), repeat=3)
+    )
+
+    for midx, fgnum1, fgnum2, fgnum3, stoich in it.product(
+        multipliers, bbs, bbs, bbs, three_type_stoichiometries
+    ):
+        if fgnum1 in (fgnum2, fgnum3) or fgnum2 == fgnum3:
+            continue
+        fgnum1_, fgnum2_, fgnum3_ = sorted(
+            (fgnum1, fgnum2, fgnum3), reverse=True
+        )
+        print(
+            f"doing m={midx}, bbs={(fgnum1_, fgnum2_, fgnum3_)}, s={stoich} "
+            f"-> m*s={(stoich[0] * midx, stoich[1] * midx, stoich[2] * midx)}"
+        )
+
+        try:
+            iterator = cgx.scram.TopologyIterator(
+                building_block_counts={
+                    bbs[fgnum1_]: midx * stoich[0],
+                    bbs[fgnum2_]: midx * stoich[1],
+                    bbs[fgnum3_]: midx * stoich[2],
+                },
+                graph_type=f"{midx * stoich[0]}-{fgnum1_}FG_"
+                f"{midx * stoich[1]}-{fgnum2_}FG_"
+                f"{midx * stoich[2]}-{fgnum3_}FG",
+                graph_set="rxx",
+            )
+            logger.info(
+                "graph iteration has %s graphs", iterator.count_graphs()
+            )
+
+        except (ZeroDivisionError, ValueError):
+            print(
+                f"      m={midx}, bbs={(fgnum1_, fgnum2_)}, s={stoich} failed"
+            )
+
+    raise SystemExit("delete me!")
 
     # One typers.
+    multipliers = range(1, 11)
     for midx, fgnum in it.product(multipliers, bbs):
         print(f"doing m={midx}, bb={fgnum}")
         try:
@@ -34,6 +76,8 @@ def main() -> None:
             )
         except (ZeroDivisionError, ValueError):
             print(f"      m={midx}, bb={fgnum} failed")
+
+    raise SystemExit("delete me!")
 
     # Two typers.
     multipliers = range(1, 13)
