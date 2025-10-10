146b1afb4f41453c9400b3e073653588020f6628
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-10-10 16:00:33 +0100

    Update make graphs script.

--- make_graphs.py
+++ make_graphs.py
@@ -16,11 +16,43 @@ def main() -> None:
         3: stk.BuildingBlock("BrCC(Br)CCBr", (stk.BromoFactory(),)),
         4: stk.BuildingBlock("BrCC(Br)CCC(Br)CCBr", (stk.BromoFactory(),)),
     }
-    multipliers = range(1, 13)
+    multipliers = range(1, 11)
+
+    # Two typers test .
+    multipliers = (3,)
+    two_type_stoichiometries = ((1, 2),)
+    for midx, fgnum1, fgnum2, stoich in it.product(
+        multipliers, bbs, bbs, two_type_stoichiometries
+    ):
+        if fgnum1 == fgnum2:
+            continue
+        fgnum1_, fgnum2_ = sorted((fgnum1, fgnum2), reverse=True)
+        print(
+            f"doing m={midx}, bbs={(fgnum1_, fgnum2_)}, s={stoich} "
+            f"-> m*s={(stoich[0] * midx, stoich[1] * midx)}"
+        )
+
+        try:
+            iterator = cgx.scram.TopologyIterator(
+                building_block_counts={
+                    bbs[fgnum2_]: midx * stoich[1],
+                    bbs[fgnum1_]: midx * stoich[0],
+                },
+                graph_type=f"{midx * stoich[0]}-{fgnum1_}FG_"
+                f"{midx * stoich[1]}-{fgnum2_}FG",
+                graph_set="test",
+            )
+            logger.info(
+                "graph iteration has %s graphs", iterator.count_graphs()
+            )
+        except (ZeroDivisionError, ValueError):
+            print(
+                f"      m={midx}, bbs={(fgnum1_, fgnum2_)}, s={stoich} failed"
+            )
 
     # One typers.
     for midx, fgnum in it.product(multipliers, bbs):
-        print(f"doing {midx}, {fgnum}")
+        print(f"doing m={midx}, bb={fgnum}")
         try:
             iterator = cgx.scram.TopologyIterator(
                 building_block_counts={
@@ -29,38 +61,43 @@ def main() -> None:
                 graph_type=f"{midx}-{fgnum}FG",
                 graph_set="rxx",
             )
-            logging.info(
+            logger.info(
                 "graph iteration has %s graphs", iterator.count_graphs()
             )
         except (ZeroDivisionError, ValueError):
-            print(f" {midx}, {fgnum} failed")
+            print(f"      m={midx}, bb={fgnum} failed")
 
     # Two typers.
-    two__type_stoichiometries = ((1, 2), (2, 3), (3, 4))
-    for midx, (fgnum1, fgnum2), stoich in it.product(
-        multipliers, bbs, bbs, two__type_stoichiometries
+    multipliers = range(1, 13)
+    two_type_stoichiometries = ((1, 2), (2, 3), (3, 4))
+    for midx, fgnum1, fgnum2, stoich in it.product(
+        multipliers, bbs, bbs, two_type_stoichiometries
     ):
         if fgnum1 == fgnum2:
             continue
-        fgnum1_, fgnum2_ = sorted(fgnum1, fgnum2)
-        print(f"doing {midx}, {(fgnum1_, fgnum2_)}, {stoich}")
-        print((stoich[0] * midx, stoich[1] * midx))
-        continue
+        fgnum1_, fgnum2_ = sorted((fgnum1, fgnum2), reverse=True)
+        print(
+            f"doing m={midx}, bbs={(fgnum1_, fgnum2_)}, s={stoich} "
+            f"-> m*s={(stoich[0] * midx, stoich[1] * midx)}"
+        )
+
         try:
             iterator = cgx.scram.TopologyIterator(
                 building_block_counts={
                     bbs[fgnum1_]: midx * stoich[0],
                     bbs[fgnum2_]: midx * stoich[1],
                 },
-                graph_type=f"{midx * stoich[0]}-{fgnum}FG_"
-                f"{midx * stoich[1]}-{fgnum}FG",
+                graph_type=f"{midx * stoich[0]}-{fgnum1_}FG_"
+                f"{midx * stoich[1]}-{fgnum2_}FG",
                 graph_set="rxx",
             )
-            logging.info(
+            logger.info(
                 "graph iteration has %s graphs", iterator.count_graphs()
             )
         except (ZeroDivisionError, ValueError):
-            print(f" {midx}, {(fgnum1, fgnum2)}, {stoich} failed")
+            print(
+                f"      m={midx}, bbs={(fgnum1_, fgnum2_)}, s={stoich} failed"
+            )
 
     raise SystemExit("delete me!")
 
