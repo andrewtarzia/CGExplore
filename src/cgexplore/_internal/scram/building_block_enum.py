7e47d8909d8deb4f860a46e94a2a02903f553663
andrewtarzia <andrew.tarzia@gmail.com>
2025-11-12 18:08:39 +0000

    Add vertex alignments to api.

--- src/cgexplore/_internal/scram/building_block_enum.py
+++ src/cgexplore/_internal/scram/building_block_enum.py
@@ -326,3 +326,60 @@ def get_custom_bb_configurations(  # noqa: C901
     logger.info(msg)
 
     return tuple(possible_dicts)
+
+
+@dataclass
+class VertexAlignment:
+    """Naming convention for vertex alignments."""
+
+    idx: int
+    vertex_dict: dict[int, int]
+
+    def __str__(self) -> str:
+        """Return a string representation of the OMMTrajectory."""
+        return (
+            f"{self.__class__.__name__}(idx={self.idx}, "
+            f"vertex_dict={self.vertex_dict})"
+        )
+
+    def __repr__(self) -> str:
+        """Return a string representation of the OMMTrajectory."""
+        return str(self)
+
+
+def get_vertex_alignments(
+    iterator: TopologyIterator,
+    allow_rotation: abc.Sequence[int],
+) -> abc.Sequence[VertexAlignment]:
+    """Get potential vertex alignment dictionaries.
+
+    Parameters:
+        iterator:
+            The graph iterator.
+
+        allow_rotation:
+            Which building blocks to allow rotation based on the number of
+            functional groups.
+
+    """
+    allow_rotation = tuple(allow_rotation)
+
+    # Get the associated vertex ids.
+    modifiable_vertices = {
+        vertex: range(fg_count) if fg_count in allow_rotation else [0]
+        for fg_count in iterator.vertex_types_by_fg
+        for vertex in iterator.vertex_types_by_fg[fg_count]
+        if fg_count > 1
+    }
+
+    if len(modifiable_vertices) == 0:
+        msg = "There are no modifiable types"
+        raise RuntimeError(msg)
+
+    iteration = it.product(*modifiable_vertices.values())
+    possible_dicts = []
+    for idx, item in enumerate(iteration):
+        vmap = dict(zip(modifiable_vertices.keys(), item, strict=True))
+        possible_dicts.append(VertexAlignment(idx=idx, vertex_dict=vmap))
+
+    return tuple(possible_dicts)
