a35ac747db922bdc61bd77f3f2df414f0bdfc152
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-12-19 15:48:47 +0000

    Update construction.

--- src/cgexplore/_internal/scram/construction.py
+++ src/cgexplore/_internal/scram/construction.py
@@ -7,13 +7,13 @@ import networkx as nx
 import numpy as np
 import stk
 import stko
+from agx import Configuration, TopologyCode
 from openmm import OpenMMException, openmm
 
 from cgexplore._internal.forcefields.assigned_system import AssignedSystem
 from cgexplore._internal.forcefields.forcefield import ForceField
 from cgexplore._internal.molecular.conformer import Conformer
 from cgexplore._internal.molecular.ensembles import Ensemble
-from cgexplore._internal.scram.topology_code import TopologyCode
 from cgexplore._internal.topologies.custom_topology import CustomTopology
 from cgexplore._internal.utilities.databases import AtomliteDatabase
 from cgexplore._internal.utilities.generation_utilities import (
@@ -23,7 +23,7 @@ from cgexplore._internal.utilities.generation_utilities import (
     yield_shifted_models,
 )
 
-from .building_block_enum import BuildingBlockConfiguration, VertexAlignment
+from .building_block_enum import VertexAlignment
 from .enumeration import TopologyIterator
 
 logging.basicConfig(
@@ -33,13 +33,12 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def get_regraphed_molecule(  # noqa: PLR0913
-    graph_type: str,
+def get_regraphed_molecule(
+    layout_type: str,
     scale: float,
     topology_code: TopologyCode,
     iterator: TopologyIterator,
-    bb_config: BuildingBlockConfiguration | None,
-    vertex_alignment: VertexAlignment | None = None,
+    configuration: Configuration | None,
 ) -> stk.ConstructedMolecule:
     """Take a graph that considers all atoms, and get atom positions.
 
@@ -51,7 +50,7 @@ def get_regraphed_molecule(  # noqa: PLR0913
       coordinates in multiple runs.
 
     Parameters:
-        graph_type:
+        layout_type:
             Which networkx layout to use (of `spring`, `kamada`).
 
         scale:
@@ -63,7 +62,7 @@ def get_regraphed_molecule(  # noqa: PLR0913
         iterator:
             The `scram` algorithm used to generate the graph and configuration.
 
-        bb_config:
+        configuration:
             The configuration of building blocks on the graph.
 
         vertex_alignment:
@@ -81,15 +80,14 @@ def get_regraphed_molecule(  # noqa: PLR0913
     constructed_molecule = try_except_construction(
         iterator=iterator,
         topology_code=topology_code,
-        building_block_configuration=bb_config,
-        vertex_alignment=vertex_alignment,
+        configuration=configuration,
         vertex_positions=None,
     )
 
     stko_graph = stko.Network.init_from_molecule(constructed_molecule)
-    if graph_type == "spring":
+    if layout_type == "spring":
         nx_positions = nx.spring_layout(stko_graph.get_graph(), dim=3)
-    elif graph_type == "kamada":
+    elif layout_type == "kamada":
         nx_positions = nx.kamada_kawai_layout(stko_graph.get_graph(), dim=3)
     else:
         raise NotImplementedError
@@ -99,13 +97,12 @@ def get_regraphed_molecule(  # noqa: PLR0913
     ).with_centroid(np.array((0.0, 0.0, 0.0)))
 
 
-def get_vertexset_molecule(  # noqa: PLR0913
-    graph_type: str | None,
+def get_vertexset_molecule(
+    layout_type: str,
     scale: float,
     topology_code: TopologyCode,
     iterator: TopologyIterator,
-    bb_config: BuildingBlockConfiguration | None,
-    vertex_alignment: VertexAlignment | None = None,
+    configuration: Configuration | None,
 ) -> stk.ConstructedMolecule:
     """Take a graph and genereate from graph vertex positions.
 
@@ -115,7 +112,7 @@ def get_vertexset_molecule(  # noqa: PLR0913
       coordinates in multiple runs.
 
     Parameters:
-        graph_type:
+        layout_type:
             Which networkx layout to use (of `spring`,
             `kamada`, `spectral`).
 
@@ -128,7 +125,7 @@ def get_vertexset_molecule(  # noqa: PLR0913
         iterator:
             The `scram` algorithm used to generate the graph and configuration.
 
-        bb_config:
+        configuration:
             The configuration of building blocks on the graph.
 
         vertex_alignment:
@@ -139,35 +136,14 @@ def get_vertexset_molecule(  # noqa: PLR0913
         A constructed molecule at (0, 0, 0).
 
     """
-    if graph_type is None:
-        return try_except_construction(
-            iterator=iterator,
-            topology_code=topology_code,
-            building_block_configuration=bb_config,
-            vertex_alignment=vertex_alignment,
-            vertex_positions=None,
-        ).with_centroid(np.array((0.0, 0.0, 0.0)))
-
-    nx_graph = topology_code.get_nx_graph()
-
-    if graph_type == "kamada":
-        nxpos = nx.kamada_kawai_layout(nx_graph, dim=3)
-    elif graph_type == "spring":
-        nxpos = nx.spring_layout(nx_graph, dim=3)
-    elif graph_type == "spectral":
-        nxpos = nx.spectral_layout(nx_graph, dim=3)
-    else:
-        raise NotImplementedError
-
-    vertex_positions = {
-        nidx: np.array(nxpos[nidx]) * float(scale)
-        for nidx in topology_code.get_nx_graph().nodes
-    }
+    vertex_positions = topology_code.get_layout(
+        layout_type=layout_type,
+        scale=scale,
+    )
     return try_except_construction(
         iterator=iterator,
         topology_code=topology_code,
-        building_block_configuration=bb_config,
-        vertex_alignment=vertex_alignment,
+        configuration=configuration,
         vertex_positions=vertex_positions,
     ).with_centroid(np.array((0.0, 0.0, 0.0)))
 
@@ -275,6 +251,7 @@ def graph_optimise_cage(  # noqa: PLR0913
         )
         ensemble.add_conformer(conformer=conformer, source="shifted")
 
+    # Try with graph positions.
     stko_graph = stko.Network.init_from_molecule(conformer.molecule)
     for i, nx_positions in enumerate(
         (
@@ -315,7 +292,7 @@ def graph_optimise_cage(  # noqa: PLR0913
             except OpenMMException:
                 logger.info("failed graph opt of %s", name)
 
-    # Try with graph positions.
+    # Try with random positions.
     rng = np.random.default_rng(seed=100)
     for attempt in range(10):
         pos_mat = rng.random(size=(conformer.molecule.get_num_atoms(), 3))
@@ -694,17 +671,20 @@ def try_except_construction(  # noqa: PLR0913
     iterator: TopologyIterator,
     topology_code: TopologyCode,
     scale_multiplier: float | None = None,
-    building_block_configuration: BuildingBlockConfiguration | None = None,
+    configuration: Configuration | None = None,
     vertex_positions: dict[int, np.ndarray] | None = None,
     vertex_alignment: VertexAlignment | None = None,
     reaction_factory: stk.ReactionFactory = stk.GenericReactionFactory(),  # noqa: B008
     optimizer: stk.Optimizer = stk.NullOptimizer(),  # noqa: B008
 ) -> stk.ConstructedMolecule:
     """Try construction with alignment, then without."""
-    if building_block_configuration is None:
+    if configuration is None:
         bbs = iterator.building_blocks
     else:
-        bbs = building_block_configuration.get_building_block_dictionary()
+        bbs = {
+            iterator.node_to_bb_map[i]: j
+            for i, j in configuration.get_node_dictionary().items()
+        }
 
     if vertex_alignment is None:
         vertex_alignments = None
@@ -720,8 +700,9 @@ def try_except_construction(  # noqa: PLR0913
                     unaligning=False
                 ),
                 # Convert to edge prototypes.
-                edge_prototypes=topology_code.edges_from_connection(
-                    iterator.get_vertex_prototypes(unaligning=False)
+                edge_prototypes=iterator.get_edges_from_topology_code(
+                    topology_code=topology_code,
+                    unaligning=False,
                 ),
                 vertex_alignments=vertex_alignments,
                 vertex_positions=vertex_positions,
@@ -742,8 +723,9 @@ def try_except_construction(  # noqa: PLR0913
                     unaligning=True
                 ),
                 # Convert to edge prototypes.
-                edge_prototypes=topology_code.edges_from_connection(
-                    iterator.get_vertex_prototypes(unaligning=True)
+                edge_prototypes=iterator.get_edges_from_topology_code(
+                    topology_code=topology_code,
+                    unaligning=True,
                 ),
                 vertex_alignments=vertex_alignments,
                 vertex_positions=vertex_positions,
